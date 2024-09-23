d2252f81dd48ecf601aca6b31405e7c810a83dfe
e-haikal <taufanasman@yahoo.com>
2024-09-23 15:31:41 +0700

    feat: finish mandatory exams (exams 1 - 5)

--- src/main/kotlin/com/dicoding/exam/exam2/Exam2.kt
+++ src/main/kotlin/com/dicoding/exam/exam2/Exam2.kt
@@ -3,8 +3,8 @@ package com.dicoding.exam.exam2
 // TODO 1
 fun calculate(valueA: Int, valueB: Int, valueC: Int?): Int {
     // Use 50 as default if valueC is null
-    val effectiveC = valueC ?: 50
-    return valueA + (valueB - effectiveC)
+    val defaultValueC = valueC ?: 50
+    return valueA + (valueB - defaultValueC)
 }
 
 // TODO 2
--- src/main/kotlin/com/dicoding/exam/exam4/Exam4Main.kt
+++ src/main/kotlin/com/dicoding/exam/exam4/Exam4Main.kt
@@ -1,10 +1,10 @@
-package com.dicoding.exam.exam4
+    package com.dicoding.exam.exam4
 
-private fun main() {
-    println(
-        """
-        My Map Result:
-        ${vehicle()}
-        """.trimIndent()
-    )
-}
+    private fun main() {
+        println(
+            """
+            My Map Result:
+            ${vehicle()}
+            """.trimIndent()
+        )
+    }
